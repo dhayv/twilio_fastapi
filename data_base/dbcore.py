d176e45bc924289eb8a33ded61db91f1a35970b3
Me <davidhyppolitedh@gmail.com>
2024-05-03 18:02:17 -0400

    added check for db file and hid db path

--- data_base/dbcore.py
+++ data_base/dbcore.py
@@ -1,10 +1,29 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import declarative_base
+from sqlalchemy.orm import declarative_base, Session
+from decouple import config
+from sqlalchemy_utils import database_exists
 
 
-db_url = "sqlite+pysqlite:///:memory"
-engine = create_engine(db_url)
+database_url = config('DATABASE_URL', default='sqlite:///./db.sqlite3')
+
+engine = create_engine(database_url)
 
 Base = declarative_base()
 
-Base.metadata.create_all(bind=engine)
+
+async def get_db():
+    db = Session(engine)
+    try:
+        yield db
+    except Exception as e:
+        db.rollback()
+        raise e
+    finally:
+        db.close()
+
+
+def create_database():
+    if database_exists(engine.url):
+        pass
+    else:
+        Base.metadata.create_all(engine)
