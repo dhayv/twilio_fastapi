d1386d06e0c7b63b17c3c6f95b14bb40f843ed87
Me <davidhyppolitedh@gmail.com>
2024-05-03 20:45:19 -0400

    completed model classes

--- models/all_classes.py
+++ models/all_classes.py
@@ -1,5 +1,5 @@
-from sqlalchemy import ForeignKey, String
-from sqlalchemy.orm import relationship, DeclarativeBase
+from sqlalchemy import ForeignKey, Enum
+from sqlalchemy.orm import Mapped, relationship, DeclarativeBase, mapped_column
 
 
 class Base(DeclarativeBase):
@@ -8,12 +8,17 @@ class Base(DeclarativeBase):
 
 class User(Base):
     __tablename__ = "user"
-    id :
-    phone_number :
+    id: Mapped[int] = mapped_column(primary_key=True)
+    phone_number: Mapped[str] = mapped_column(unique=True)
+
+    addresses: Mapped["Message"] = relationship(back_populates="user")
+
 
 class Message(Base):
     __tablename__ = "message"
-    id :
-    message :
-    direction :
-    user_id :
+    id: Mapped[int] = mapped_column(primary_key=True)
+    message: Mapped[str]
+    direction: Mapped[Enum]
+    user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+
+    addresses: Mapped["User"] = relationship(back_populates="message")
