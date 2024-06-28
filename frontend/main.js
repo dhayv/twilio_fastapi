34883cec1b33a1c4e7873435eb7cad67a1db5f4f
Dhayv <davidhyppolitedh@gmail.com>
2024-06-28 15:12:49 -0400

    code: clean post/get response, added functionality for text area

--- frontend/main.js
+++ frontend/main.js
@@ -1,7 +1,5 @@
-import axios from 'axios';
-
 const api = axios.create({
-  baseURL: 'http://127.0.0.1:8000' 
+  baseURL: 'http://127.0.0.1:8000/' 
 });
 
 
@@ -9,10 +7,46 @@ document.getElementById("formData").addEventListener('submit', function(event){
     event.preventDefault();
 
 
-    const inputValue = document.getElementById("userInput").value;
+    const inputValue = document.getElementById("userInput").value.trim();
 
-    const data = { input: inputValue}
+    const data = { message: inputValue}
 
-    api.post("", data)
+    // send user question
+    api.post("/usersearch", data)
+      .then((response) => {
+        console.log(response.data);
+        const request_id = response.data.request_id;
+
+        // retreive ai response
+        return api.get(`/response`, { params: { request_id: request_id } });
+
+      })
+
+    .then((response) => {
+      console.log(response.data)
+      const responseInput = document.getElementById("responseInput");
+      responseInput.value = response.data.response
+      adjustTexareaHeight(responseInput)
+
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 
 });
+
+const userInput = document.getElementById('userInput');
+const hiddenSpan = document.getElementById('hiddenSpan');
+
+userInput.addEventListener('input', function() {
+  hiddenSpan.textContent = userInput.value;
+  adjustTexareaHeight(userInput)
+});
+
+function adjustTexareaHeight(textarea) {
+  textarea.style.height = 'auto'; // Reset the height
+  textarea.style.height = (textarea.scrollHeight + 2) + 'px'; // Adjust based on scroll height
+}
+
+const responseInput = document.getElementById('responseInput');
+adjustTexareaHeight(responseInput);
